36def7f9cf4316bb94a11bdf17f33ba09db81399
ntrajic <ntrajic@acm.org>
2024-03-13 02:30:47 +0000

    bounded boxes around faces: face detection

--- face-detection.py
+++ face-detection.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2  # NOTE: run this face detection w/ boxes service on WINDOWS platform, X11 has problem w/ QT xcb plugin loading and initialization + opencv lib requires recompile for pluging to load properly
 
 face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
@@ -12,7 +12,7 @@ for (x, y, w, h) in faces:
 
 cv2.imshow('Group Photo', group_photo)
 
-# cv2.imwrite('group-photo-copy.webp', gray_photo)
+#cv2.imwrite('group-photo-copy.webp', gray_photo)     # for creating the gray photo from a color photo
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
